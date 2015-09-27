cc12993a8f4c94cb31cf43965b770fa369f5a15a
Nguyễn Quý Tú <quytutlu@gmail.com>
2015-09-27 15:37:04 +0700

    được hơn 5 thiết bị
    Hiện tại có thể điều khiển được hơn 5 thiết bị

--- Internet.ino
+++ Internet.ino
@@ -1,7 +1,5 @@
 #include <SPI.h>
 #include <Ethernet.h>
-#include <JsonParser.h>
-//#include <ArduinoJson.h>
 byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
 boolean received = false;
 EthernetClient client;
@@ -12,8 +10,6 @@ IPAddress server(52, 68, 172, 187);
 unsigned long lastConnectionTime = 0;
 boolean lastConnected = false;
 String TrangThai;
-JsonParser<32> parser;
-JsonHashTable hashTable;
 const unsigned long postingInterval = 500;  // delay between updates, in milliseconds
 
 void setup() {
@@ -37,9 +33,10 @@ void loop() {
   flag = false;
   while (client.available()) {
     char c = client.read();
-    if (c == '{')
+    if (c == '{'||c=='}')
     {
       flag = true;
+      continue;
     }
     if (flag)
     {
@@ -47,11 +44,7 @@ void loop() {
     }
     received = true;
   }
-  //char *json = new char[str.length() + 1];
-  //strcpy(json, str.c_str());
-  Serial.print(str);
-  PhanTichJson(&str[0u]);
-  //delete[]json;
+  ParserData(str);
   if (received)
   {
     client.stop();
@@ -70,22 +63,10 @@ void loop() {
   lastConnected = client.connected();
   delay(500);
 }
-JsonArray list;
-JsonHashTable ThietBi;
-void PhanTichJson(char *json)
-{
-  hashTable = parser.parseHashTable(json);
-  if (!hashTable.success())
-  {
-    //Serial.println("fail");
-    return;
-  }
-  list = hashTable.getArray("list");
-  for (int i = 0; i < list.getLength(); i++)
-  {
-    ThietBi = list.getHashTable(i);
-    TrangThai = ThietBi.getString("TrangThai");
-    //Serial.println(TrangThai);
+void ParserData(String data){
+  for(int i=0;i<data.length();i+=4){
+    //Serial.println(data.substring(i,i+4));
+    TrangThai=data.substring(i,i+4);
     // điền mã thiết bị trên hệ thống smarthometl.com
     //nếu mã thiết bị + 0 thì sẽ là tắt
     //nếu mã thiết bị +1 thì sẽ là bật
@@ -94,63 +75,60 @@ void PhanTichJson(char *json)
     //Đối với Arduino thì chỉ có thể điều khiển được 5 thiết bị vì thế bạn hãy cấu hình trên hệ thống smarthometl.com chỉ 5 thiết bị thôi nhé!
     if (TrangThai == "hqr1")
     {
-      digitalWrite(7, LOW);    //Tắt thiết bị 1 tạicổng số 7
+      digitalWrite(7, LOW);    //Bật thiết bị 1 tạicổng số 7
       continue;
     }
     if (TrangThai == "hqr0")
     {
-      digitalWrite(7, HIGH);    //Bật thiết bị 1 tạicổng số 7
+      digitalWrite(7, HIGH);    //Tắt thiết bị 1 tạicổng số 7
       continue;
     }
     
     if (TrangThai == "krq1")
     {
-      digitalWrite(6, LOW);  //Tắt thiết bị 2 tạicổng số 6
+      digitalWrite(6, LOW);  //Bật thiết bị 2 tạicổng số 6
       continue;
     }
     if (TrangThai == "krq0")
     {
-      //Serial.println("Den tuyp bat");
-      digitalWrite(6, HIGH);  //Bật thiết bị 2 tạicổng số 6
+      digitalWrite(6, HIGH);  //Tắt thiết bị 2 tạicổng số 6
       continue;
     }
     
     if (TrangThai == "cfj1")
     {
-      digitalWrite(5, LOW);    //Tắt thiết bị 3 tạicổng số 5
+      digitalWrite(5, LOW);    //Bật thiết bị 3 tạicổng số 5
       continue;
     }
     if (TrangThai == "cfj0")
     {
-      //Serial.println("Den quat dien 1 bat");
-      digitalWrite(5, HIGH);  //Bật thiết bị 3 tạicổng số 5
+      digitalWrite(5, HIGH);  //Tắt thiết bị 3 tạicổng số 5
       continue;
     }
     
     if (TrangThai == "dpf1")
     {
-      digitalWrite(4, LOW);  //Tắt thiết bị 4 tạicổng số 4
+      digitalWrite(4, LOW);  //Bật thiết bị 4 tạicổng số 4
       continue;
     }
     if (TrangThai == "dpf0")
     {
-      //Serial.println("Den quat dien 2 bat");
-      digitalWrite(4, HIGH);  //Bật thiết bị 4 tạicổng số 4
+      digitalWrite(4, HIGH);  //Tắt thiết bị 4 tạicổng số 4
       continue;
     }
     
     if (TrangThai == "")
     {
-      digitalWrite(3, LOW);  //Tắt thiết bị 5 tạicổng số 3
+      digitalWrite(3, LOW);  //Bật thiết bị 5 tạicổng số 3
       continue;
     }
     if (TrangThai == "")
     {
-      //Serial.println("Den tivi bat");
-      digitalWrite(3, HIGH);  //Bật thiết bị 5 tạicổng số 3
+      digitalWrite(3, HIGH);  //Tắt thiết bị 5 tạicổng số 3
       continue;
     }
   }
+  return;
 }
 void httpRequest() {
   if (client.connect(server, 80)) {
